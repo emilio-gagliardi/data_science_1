ebf1a647d83401337edd38e7346f347facb1f646
emilio <emilio.gagliardi@gmail.com>
2022-11-08 18:47:01 -0700

    added comment

---
+++ src/data_science_1/maths.py
@@ -0,0 +1,5 @@
+def addup(x, y):
+    """This adds up numbers"""
+    return x+y
+
+# testing comments
---
+++ tests/test_maths.py
@@ -0,0 +1,4 @@
+import data_science_1.maths
+
+def test_addup():
+    assert data_science_1.maths.addup(3,4) == 7
